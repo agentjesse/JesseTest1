26ac1532e0c7a881ab69c2a81f9c23defc745d81
Jesse <31576321+agentjesse@users.noreply.github.com>
2023-02-26 19:17:24 -0500

    Initial commit

---
+++ app/src/main/java/com/example/jessetest1/Activity2.kt
@@ -0,0 +1,25 @@
+package com.example.jessetest1
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.jessetest1.databinding.Activity2Binding
+
+class Activity2 : AppCompatActivity() {
+    //below is for the recent View Binding feature
+    private lateinit var bind: Activity2Binding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //setContentView(R.layout.activity_2)
+        //below is for the recent View Binding feature
+        bind = Activity2Binding.inflate(layoutInflater)
+        setContentView(bind.root)
+
+        //get string type data from previous activity, access with same name key
+        val bundle: Bundle? = intent.extras
+
+        if (bundle!!.getString("playerName")!!.isNotEmpty()){
+            bind.welcomePlayerText.text = "Welcome ${bundle.getString("playerName")}, prepare to be amazed"
+        }
+    }
+}
---
+++ app/src/main/java/com/example/jessetest1/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.example.jessetest1
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+//below is for View Binding feature
+import com.example.jessetest1.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    //below is for the recent View Binding feature
+    private lateinit var bind: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //setContentView(R.layout.activity_main)
+        //below is for the recent View Binding feature
+        bind = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(bind.root)
+
+        //this button just yells at user with a toast
+        bind.yellBtn.setOnClickListener {
+            //Log.i("Main Activity .yellBtn","aaaahhhhhHHHH!")
+            //show a short toast message
+            Toast.makeText(this, "aaaahhhhhHHHH!", Toast.LENGTH_SHORT).show()
+        }
+
+        //handle moving to next activity while passing on the player's name
+        bind.enterDreamlandBtn.setOnClickListener {
+        //log player's name from the playerNameText EditText element
+        //Log.i("Main Activity's .playerNameText EditText element", bind.playerNameText.text.toString() )
+
+        //navigate to Activity2 and send some data from this activity to there
+        val intent = Intent(this,Activity2::class.java)
+        intent.putExtra("playerName",bind.playerNameText.text.toString())
+        startActivity(intent)
+
+        }
+
+    }
+}
